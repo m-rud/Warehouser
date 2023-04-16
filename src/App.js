7c5237c471604c50a977b23497b1d39001100c2e
MichalRudolf <michal.rudolff@gmail.com>
2023-04-17 01:42:43 +0200

    feat: implement Astar algorithm

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Pathfind from "./components/Pathfind"
+
+const App = () => {
+    return (
+        <div>
+            <Pathfind/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/astarAlgorithm/astar.js
@@ -0,0 +1,68 @@
+function Astar(startNode, endNode)
+{
+    let openSet = [];
+    let closedSet = [];
+    let path = [];
+
+    openSet.push(startNode);
+    while(openSet.length > 0) {
+        let leastIndex = 0;
+        for(let i =0; i < openSet.length; i++){
+            if(openSet[i].f < openSet[leastIndex].f){
+                leastIndex = i;
+            }
+        }
+
+        let current = openSet[leastIndex];
+
+        if(current === endNode)
+        {
+            let temp = current;
+            path.push(temp);
+            while(temp.previous){
+                path.push(temp.previous);
+                temp = temp.previous;
+            }
+            console.log("Done! Path Found!");
+            return path;
+        }
+
+        openSet = openSet.filter(elt => elt !== current);
+        closedSet.push(current);
+
+        let neighbours = current.neighbours;
+        for(let i = 0; i < neighbours.length; i++){
+            let neighbour = neighbours[i];
+            if(!closedSet.includes(neighbour)){
+                let tempG = current.g + 1;
+                let newPath = false;
+                if(openSet.includes(neighbour)){
+                    if(tempG < neighbour.g){
+                        neighbour.g = tempG;
+                        newPath = true;
+                    }
+                } else {
+                        neighbour.g = tempG;
+                        newPath = true;
+                        openSet.push(neighbour);
+                    }
+
+                    if(newPath){
+                        neighbour.h = heuristic(neighbour, endNode);
+                        neighbour.f = neighbour.g + neighbour.f;
+                        neighbour.previous = current;
+                    }
+                
+            }
+        }
+    }
+    return {path, error: "No path found!"}
+}
+
+function heuristic(a,b){
+    //let d = Math.abs(b.x - a.x) + Math.abs(b.y - a.y);
+    let d = Math.abs(a.x - a.y) + Math.abs(b.x - b.y);
+    return d;
+}
+
+export default Astar;
