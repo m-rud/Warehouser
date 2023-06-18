ea08ba47a7ec43f321f0762e759a0451e96c4167
MichalRudolf <michal.rudolff@gmail.com>
2023-06-18 22:48:28 +0200

    improve rwd and design

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,9 +1,14 @@
 import { Html5QrcodeScanner, Html5Qrcode } from "html5-qrcode";
 import { useEffect, useState } from "react";
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+import CloseIcon from '@mui/icons-material/Close';
 
-function Scanner() {
+const Scanner = () => {
 
     const [scanResult, setScanResult] = useState(null);
+    const [scanning, setScanning] = useState(false);
 
     let qrCode;
     const qrCodeSuccessCallback = (decodedText, decodedResult) => {
@@ -13,40 +18,44 @@ function Scanner() {
             qrCode.clear();
         })
     };
-    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+    const config = { fps: 10, qrbox: { width: 200, height: 200 } };
 
     useEffect(() => {
-        // File based scanning
-        const fileinput = document.getElementById('qr-input-file');
-        fileinput.addEventListener('change', e => {
-        if (e.target.files.length == 0) {
-            return;
-        }
-        const imageFile = e.target.files[0];
-        qrCode.scanFile(imageFile, /* showImage= */false)
-        .then(qrCodeMessage => {
-            console.log(qrCodeMessage);
-        })
-        .catch(err => {
-            console.log(`Error scanning file. Reason: ${err}`)
-        });
-        });
+
     }, []);
 
     const run = () => {
-        setScanResult(null);
-        qrCode = new Html5Qrcode("reader");
-        qrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback, (error) => console.log(error));
+        if (!scanning) {
+            setScanResult(null);
+            qrCode = new Html5Qrcode("reader");
+            qrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback, (error) => console.log(error));
+            setScanning(true);
+        }
+        else {
+            qrCode.stop().then(() => {
+                qrCode.clear();
+            })
+            setScanning(false);
+        }
+        
     }
-
+//<button onClick={run} style={{ position: 'fixed', bottom: '10px', right: '10px'}}>QR</button>
     return (
         <div>
-            <button onClick={run} style={{ position: 'fixed', bottom: '10px', right: '10px'}}>QR</button>
+            <Box
+            m={2}
+            position="fixed"
+            bottom="0px"
+            right="0px"
+            >
+            <Fab color={scanning ? "warning" : "primary"} aria-label="add" onClick={run} >
+                {scanning ? <CloseIcon/> : <QrCodeScannerIcon />}
+            </Fab>
+            </Box>
             {scanResult
             ? <div>QR result: {scanResult}</div>
             : <div id="reader"></div>
             }
-            <input type="file" id="qr-input-file" accept="image/*"></input>
         </div>
     );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import Pathfind from "../components/Pathfind"
+import Scanner from "../components/Scanner"
 
 function Home() {
     return (
         <div>
             <Pathfind />
+            
         </div>
     );
 }
