dfcdf600eb465a1d4f37a11459c7494fe192a9c5
Krzysztof Bogaczyk <bogaczyk.krzysztof00@gmail.com>
2023-06-19 02:34:51 +0200

    login errors | logout buton

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,8 @@ import { collection, getDocs, query, where } from "firebase/firestore"
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"
 import { db } from "../firebase"
 
+import logoutImage from "../assets/logout.png"
+
 import '../App.css';
 
 function Navbar() {
@@ -40,7 +42,8 @@ function Navbar() {
                     <Link key={route.label} to={route.path}><div className={location.pathname === route.path ? "navbarButton-visited" : "navbarButton"}>{route.label}</div></Link>
                 ))}
             </div>
-            <button onClick={() => logOut()} >Logout</button>
+            {/* <button onClick={() => logOut()} variant="contained"></button> */}
+            <img onClick={() => logOut()} src={logoutImage} height={"80%"} style={{ marginTop: "5px" }} />
         </div>
     );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -5,33 +5,61 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { db } from "../firebase"
 
 function Login() {
+    const [emailErrorMessage, setEmailErrorMessage] = useState('');
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
     const [credentials, setCredentials] = useState({});
 
+    const error = {
+        color: "red",
+        fontSize: "15px",
+        marginTop: "5px",
+    }
+
     const handleChange = (event) => {
+        if (event.target.name == "email") setEmailErrorMessage("");
+        if (event.target.name == "password") setPasswordErrorMessage("");
         const name = event.target.name;
         const value = event.target.value;
         setCredentials(values => ({ ...values, [name]: value }))
     }
 
-    const handleSubmit = async (event) => {
-        console.log(credentials);
+    const handleSubmit = (event) => {
+        setEmailErrorMessage("");
+        setPasswordErrorMessage("");
+
         event.preventDefault();
+        if (!credentials.email) {
+            setEmailErrorMessage("To pole jest wymagane.");
+            return;
+        }
+        if (!credentials.password) {
+            setPasswordErrorMessage("To pole jest wymagane.");
+            return;
+        }
+        login();
+    }
+
+    const login = async (event) => {
         const auth = getAuth();
-        signInWithEmailAndPassword(auth, credentials.email, credentials.password);
+        signInWithEmailAndPassword(auth, credentials.email, credentials.password).catch((error) => {
+            setEmailErrorMessage("Brak użytkownika o podanym email lub haśle.");
+        })
         setCredentials([]);
     }
 
     return (
         <div style={{ width: "100%", height: "100%", display: "flex", flexFlow: "column", justifyContent: "center", backgroundColor: "yellowgreen" }}>
-            <div style={{ width: "70%", height: "35%", margin: "10px auto", borderRadius: "25px", boxShadow: "5px 10px 18px #888888", backgroundColor: "white" }}>
+            <div style={{ width: "70%", margin: "10px auto", borderRadius: "25px", boxShadow: "5px 10px 18px #888888", backgroundColor: "white" }}>
                 <form style={{ display: "flex", flexFlow: "column", padding: "20px" }} onSubmit={handleSubmit}>
                     <label style={{ textAlign: "left", width: "100%", marginBottom: "20px" }}>Email:<br></br>
                         <input style={{ marginTop: "10px", width: "98%" }} type="email" name="email" value={credentials.email || ""} onChange={handleChange} />
+                        {emailErrorMessage && <div style={error}>{emailErrorMessage}</div>}
                     </label>
                     <label style={{ textAlign: "left", width: "100%", marginBottom: "20px" }}>Hasło:<br></br>
                         <input style={{ marginTop: "10px", width: "98%" }} type="password" name="password" value={credentials.password || ""} onChange={handleChange} />
+                        {passwordErrorMessage && <div style={error}>{passwordErrorMessage}</div>}
                     </label>
-                    <input style={{ margin: "30px auto 0", width: "80%", height: "50px", fontSize: "21px", fontWeight: "bold", backgroundColor: "lightgreen" }} type="submit" value="Zaloguj" />
+                    <input style={{ margin: "10px auto 0", width: "80%", height: "50px", fontSize: "21px", fontWeight: "bold", backgroundColor: "lightgreen" }} type="submit" value="Zaloguj" />
                 </form>
             </div>
         </div>
